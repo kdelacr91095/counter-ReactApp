da22cd1381e25aa57510c0419edd61607bc8b5ab
Krishel Dela Cruz <kdelacr91095@yahoo.com>
2019-01-11 16:16:12 -0600

    React practice

---
+++ src/components/counter.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+	state = {
+		value: this.props.value
+	};
+
+	handleIncrement = () => {
+		this.setState({ value: this.state.count + 1 });
+	};
+
+	render() {
+		return (
+			<div>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button
+					onClick={this.handleIncrement}
+					className="btn btn-secondary btn-sm"
+				>
+					{" "}
+					Increment
+				</button>
+				<button
+					onClick={() => this.props.OnDelete(this.props.id)}
+					className="button btn btn-danger btn-sm m-2"
+				>
+					{" "}
+					Delete
+				</button>
+			</div>
+		);
+	}
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.value == 0 ? "warning" : "primary";
+		return classes;
+	}
+
+	formatCount() {
+		const { value } = this.state;
+		return value == 0 ? <h1>Zero</h1> : value;
+	}
+}
+
+export default Counter;
